48363ac3df65bd2057c4484fde0cf430c9f7c5a4
Tiago Costa <tiago.costa@elastic.co>
2026-02-17 20:32:02 +0000

    skip flaky suite (#253454)

--- x-pack/platform/plugins/shared/index_management/public/application/components/component_templates/__jest__/client_integration/component_template_create.test.tsx
+++ x-pack/platform/plugins/shared/index_management/public/application/components/component_templates/__jest__/client_integration/component_template_create.test.tsx
@@ -94,6 +94,7 @@ describe('<ComponentTemplateCreate />', () => {
   });
 
   // FLAKY: https://github.com/elastic/kibana/issues/253349
+  // FLAKY: https://github.com/elastic/kibana/issues/253454
   describe.skip('Step: Review and submit', () => {
     const COMPONENT_TEMPLATE_NAME = 'comp-1';
     const SETTINGS = { number_of_shards: 1 };
