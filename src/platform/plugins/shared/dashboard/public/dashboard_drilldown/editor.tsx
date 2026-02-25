viduni94/kibana

commit d1c8b9d447a1aea9ea4e2a541a38780965ff0e7c

[dashboards as code] Refactor drilldowns UI code to use drilldowns schema types (#252976)
Follow up to https://github.com/elastic/kibana/pull/249445
Part of https://github.com/elastic/kibana/issues/239425

Kibana-presentation team is working on "Dashboards as code" project.
https://github.com/elastic/kibana/pull/249445 created a schema for
drilldowns and updated Dashboard REST API to use new schema defined
shape for drilldowns. https://github.com/elastic/kibana/pull/249445 did
not touch client code - leaving client code using old shape. This PR
takes the next step and refactors client code to use new schema defined
drilldowns shape.

### Overview of changes
* Decouples drilldowns from dynamic actions
* Moves registryDrilldown from uiActionsEnhanced plugin to embeddables
plugin
* Moves DrilldownsManager UI components from uiActionsEnhanced plugin to
embeddables plugin. The effort tried to change as little as possible in
DrilldownsManager UI components.
* Removes dashboardEnhanced plugin
* Moves createDrilldowns action and manageDrilldowns action registration
into embeddable plugin
    * Moves dashboard drilldown registation into drilldown plugin
* Removes emebbeddableEnhanced plugin
* Replaces `HasDynamicActions` interface with `HasDrilldown` interface
and moves this interface into embeddable plugin
* Replaces `initializeDynamicActionsManager` with
`initializeDrilldownsManager`
* Moves `initializeDrilldownsManager` as parameter passed to
`buildEmbeddable` instead of exposing via plugin start contract.
   * Updates all embeddables to `initializeDrilldownsManager` 
* Moves DiscoverDrilldown to embeddables plugin `registerDrilldown`
* Moves URLDrilldown to embeddables plugin `registerDrilldown`  

### What this PR does not do
The uiActionsEnhanced plugin can be removed now that drilldowns no
longer use dynamic actions. This is clean up work that will occur in
future efforts. URLDrilldown is still very intertwined with

src/platform/plugins/shared/dashboard/public/dashboard_drilldown/editor.tsx
@@ -1,0 +1,179 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { useEffect, useMemo, useState } from 'react';
+import type { EuiComboBoxOptionOption } from '@elastic/eui';
+import { EuiFormRow, EuiComboBox, EuiSkeletonText } from '@elastic/eui';
+
+import { i18n } from '@kbn/i18n';
+import type { DrilldownEditorProps } from '@kbn/embeddable-plugin/public';
+import useDebounce from 'react-use/lib/useDebounce';
+import type { DashboardDrilldownState } from '../../server';
+import { findService } from '../dashboard_client';
+import { DEFAULT_DASHBOARD_NAVIGATION_OPTIONS } from '../../common/page_bundle_constants';
+import { DashboardNavigationOptionsEditor } from '../dashboard_navigation/options_editor';
+
+export const DashboardDrilldownEditor = (props: DrilldownEditorProps<DashboardDrilldownState>) => {
+  const [options, setOptions] = useState<Array<EuiComboBoxOptionOption<string>>>([]);
+  const [isLoadingOptions, setIsLoadingOptions] = useState(false);
+  const [initialDashboardOption, setInitialDashboardOption] = useState<
+    EuiComboBoxOptionOption<string> | undefined
+  >();
+  const [isLoadingInitialDashboard, setIsLoadingInitialDashboard] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+  const [searchString, setSearchString] = useState<string | undefined>();
+  const [debouncedSearchString, setDebouncedSearchString] = useState<string | undefined>();
+
+  useDebounce(
+    () => {
+      setDebouncedSearchString(searchString);
+    },
+    500,
+    [searchString]
+  );
+
+  useEffect(() => {
+    let canceled = false;
+    setIsLoadingOptions(true);
+
+    findService
+      .search({
+        search: debouncedSearchString ?? '',
+        per_page: 100,
+      })
+      .then((results) => {
+        if (canceled) {
+          return;
+        }
+
+        setOptions(
+          results.dashboards.map(({ id, data }) => ({
+            value: id,
+            label: data.title,
+          }))
+        );
+        setIsLoadingOptions(false);
+      });
+
+    return () => {
+      canceled = true;
+    };
+  }, [debouncedSearchString]);
+
+  useEffect(() => {
+    const initialDashboardId = props.state.dashboard_id;
+    if (!initialDashboardId) {
+      return;
+    }
+
+    let canceled = false;
+    setIsLoadingInitialDashboard(true);
+    findService.findById(initialDashboardId).then((result) => {
+      if (canceled) {
+        return;
+      }
+      setIsLoadingInitialDashboard(false);
+      // handle case when destination dashboard no longer exists
+      if (result.status === 'error') {
+        setError(
+          result.notFound
+            ? i18n.translate('dashboard.drilldown.errorDestinationDashboardIsMissing', {
+                defaultMessage:
+                  "Destination dashboard (''{dashboardId}'') no longer exists. Choose another dashboard.",
+                values: {
+                  dashboardId: initialDashboardId,
+                },
+              })
+            : result.error.message
+        );
+        props.onChange({ ...props.state, dashboard_id: undefined });
+        return;
+      }
+
+      setInitialDashboardOption({
+        value: initialDashboardId,
+        label: result.attributes.title,
+      });
+    });
+
+    return () => {
+      canceled = true;
+    };
+
+    // run on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const mergedOptions = useMemo(() => {
+    if (!initialDashboardOption || initialDashboardOption.value !== props.state.dashboard_id) {
+      return options;
+    }
+
+    const hasInitialDashboard = options.some(({ value }) => value === initialDashboardOption.value);
+    return hasInitialDashboard ? options : [initialDashboardOption, ...options];
+  }, [initialDashboardOption, options, props.state]);
+
+  const selectedOptions = useMemo(() => {
+    if (!props.state.dashboard_id) {
+      return undefined;
+    }
+    const selectedOption = mergedOptions.find(({ value }) => value === props.state.dashboard_id);
+    return selectedOption ? [selectedOption] : undefined;
+  }, [mergedOptions, props.state.dashboard_id]);
+
+  const navigationOptions = useMemo(() => {
+    return {
+      ...DEFAULT_DASHBOARD_NAVIGATION_OPTIONS,
+      ...props.state,
+    };
+  }, [props.state]);
+
+  return (
+    <EuiSkeletonText isLoading={isLoadingInitialDashboard}>
+      <EuiFormRow
+        label={i18n.translate(
+          'dashboard.components.DashboardDrilldownConfig.chooseDestinationDashboard',
+          {
+            defaultMessage: 'Choose destination dashboard',
+          }
+        )}
+        fullWidth
+        isInvalid={!!error}
+        error={error}
+      >
+        <EuiComboBox<string>
+          async
+          selectedOptions={selectedOptions}
+          options={mergedOptions}
+          onChange={(nextSelectedOptions) => {
+            props.onChange({ ...props.state, dashboard_id: nextSelectedOptions?.[0]?.value });
+            if (error) {
+              setError(undefined);
+            }
+          }}
+          onSearchChange={setSearchString}
+          isLoading={isLoadingOptions}
+          singleSelection={{ asPlainText: true }}
+          fullWidth
+          data-test-subj={'dashboardDrilldownSelectDashboard'}
+          isInvalid={!!error}
+        />
+      </EuiFormRow>
+      <DashboardNavigationOptionsEditor
+        options={navigationOptions}
+        onOptionChange={(changedState) => {
+          props.onChange({
+            ...props.state,
+            ...changedState,
+          });
+        }}
+      />
+    </EuiSkeletonText>
+  );
+};

x-pack/platform/plugins/shared/lens/public/drilldowns/editor.tsx
@@ -1,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import type { EuiSwitchEvent } from '@elastic/eui';
+import { EuiFormRow, EuiSwitch } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+import type { DrilldownEditorProps } from '@kbn/embeddable-plugin/public';
+import type { DiscoverDrilldownState } from '../../server';
+import type { SetupContext } from './get_discover_drilldown';
+
+export const DiscoverDrilldownEditor: React.FC<
+  DrilldownEditorProps<DiscoverDrilldownState, SetupContext>
+> = (props) => {
+  return (
+    <EuiFormRow hasChildLabel={false}>
+      <EuiSwitch
+        id="openInNewTab"
+        name="openInNewTab"
+        label={i18n.translate('xpack.lens.app.exploreDataInDiscoverDrilldown.newTabConfig', {
+          defaultMessage: 'Open in new tab',
+        })}
+        checked={props.state.open_in_new_tab ?? false}
+        onChange={(event: EuiSwitchEvent) =>
+          props.onChange({ ...props.state, open_in_new_tab: event.target.checked })
+        }
+        data-test-subj="openInDiscoverDrilldownOpenInNewTab"
+      />
+    </EuiFormRow>
+  );
+};

commit 0bd14fa562a7719df17dd098bee0684a9a56f593

[Dashboard Drilldown] fix Selected destination dashboard value vanishes from dropdown after selection (#254997)
Closes https://github.com/elastic/kibana/issues/254608

Regression caused by https://github.com/elastic/kibana/pull/252976

### Testing
* install sample web logs
* Change search limit to "1" at
https://github.com/elastic/kibana/blob/main/src/platform/plugins/shared/dashboard/public/dashboard_drilldown/editor.tsx#L48
* Create a new dashboard and save as "empty"
* Create another new dashboard
* Add a panel that supports dashboard drilldowns, like lens or maps
* add a dashboard drilldown.
* In dashboard select drop down, type "empty" and selected "empty"
dashboard
* verify dashboard does not disappear when selected.

---------

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

src/platform/plugins/shared/dashboard/public/dashboard_drilldown/editor.tsx
@@ -22,7 +22,7 @@ import { DashboardNavigationOptionsEditor } from '../dashboard_navigation/option
 export const DashboardDrilldownEditor = (props: DrilldownEditorProps<DashboardDrilldownState>) => {
   const [options, setOptions] = useState<Array<EuiComboBoxOptionOption<string>>>([]);
   const [isLoadingOptions, setIsLoadingOptions] = useState(false);
-  const [initialDashboardOption, setInitialDashboardOption] = useState<
+  const [selectedOption, setSelectedOption] = useState<
     EuiComboBoxOptionOption<string> | undefined
   >();
   const [isLoadingInitialDashboard, setIsLoadingInitialDashboard] = useState(false);
@@ -96,7 +96,7 @@ export const DashboardDrilldownEditor = (props: DrilldownEditorProps<DashboardDr
         return;
       }
 
-      setInitialDashboardOption({
+      setSelectedOption({
         value: initialDashboardId,
         label: result.attributes.title,
       });
@@ -109,23 +109,6 @@ export const DashboardDrilldownEditor = (props: DrilldownEditorProps<DashboardDr
     // run on mount
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const mergedOptions = useMemo(() => {
-    if (!initialDashboardOption || initialDashboardOption.value !== props.state.dashboard_id) {
-      return options;
-    }
-
-    const hasInitialDashboard = options.some(({ value }) => value === initialDashboardOption.value);
-    return hasInitialDashboard ? options : [initialDashboardOption, ...options];
-  }, [initialDashboardOption, options, props.state]);
-
-  const selectedOptions = useMemo(() => {
-    if (!props.state.dashboard_id) {
-      return undefined;
-    }
-    const selectedOption = mergedOptions.find(({ value }) => value === props.state.dashboard_id);
-    return selectedOption ? [selectedOption] : undefined;
-  }, [mergedOptions, props.state.dashboard_id]);
 
   const navigationOptions = useMemo(() => {
     return {
@@ -149,9 +132,10 @@ export const DashboardDrilldownEditor = (props: DrilldownEditorProps<DashboardDr
       >
         <EuiComboBox<string>
           async
-          selectedOptions={selectedOptions}
-          options={mergedOptions}
+          selectedOptions={selectedOption ? [selectedOption] : undefined}
+          options={options}
           onChange={(nextSelectedOptions) => {
+            setSelectedOption(nextSelectedOptions?.[0]);
             props.onChange({ ...props.state, dashboard_id: nextSelectedOptions?.[0]?.value });
             if (error) {
               setError(undefined);