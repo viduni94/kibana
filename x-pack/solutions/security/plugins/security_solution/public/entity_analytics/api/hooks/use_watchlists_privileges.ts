c8dd11db33cccb63faa150d44a9f7bf3c9258cb1
Charlotte Alexandra Wilson <CAWilson94@users.noreply.github.com>
2026-02-27 13:42:04 +0000

    WL Management Table  (#253301)
    ## Summary
    Adds Watchlists Management table with loading, error, and
    insufficient‑privileges states.
    
    ### What's included
    - Watchlist management table with available columns 
    - Error and loading states/callouts
    - Missing‑privileges callout (same privileges as PrivMon for now)
    
    ### What's not included
    - Functionality of the action button - tracking
    [here](https://github.com/orgs/elastic/projects/668/views/53?sliceBy%5Bvalue%5D=EA+9.4+Sprint+5&pane=issue&itemId=3983362984&issue=elastic%7Csecurity-team%7C16108)
    - Number of entities column values (tracking
    [here](https://github.com/elastic/security-team/issues/16111))
    - Sources column values (tracking
    [here](https://github.com/elastic/security-team/issues/16112))
    - Create watchlist button (tracking
    [here](https://github.com/elastic/security-team/issues/15466))
    
    ### Screenshots 
    <details>
    <summary>Screenshots</summary>
    
    **With Data**
    <img width="1335" alt="elastic"
    src="https://github.com/user-attachments/assets/45646801-992f-43a1-98d0-37199bb31d81"
    />
    
    **Without Data**
    <img width="1339" height="885" alt="© elastic"
    src="https://github.com/user-attachments/assets/dba9d77d-8fb9-4d0e-9fd9-a164bd48836a"
    />
    
    **Insufficient Privileges Callout**
    <img width="1340" height="588" alt="Watchlists Management"
    src="https://github.com/user-attachments/assets/939de787-f2e3-4c81-a06a-89e222c80c31"
    />
    
    **Privileges Endpoint Error State Callout**
    <img width="2012" height="708" alt="image"
    src="https://github.com/user-attachments/assets/f9ba2931-d105-4d8b-99a4-40339f8b6676"
    />
    
    
    **Error State Callout**
    <img width="1822" height="899" alt="Watchlists Management"
    src="https://github.com/user-attachments/assets/4deeb202-54b9-4eba-b572-550b53f762de"
    />
    
    </details>
    
    ### How to test it?
    #### Happy Path 
    1. Open kibana and navigate to watchlists management page - initially
    empty
    2. Use dev tools to add a few watchlists, example commands below 
    3. Navigate to watchlists page, table should have all columns filled
    **bar** _Number of entities_ and _sources_
    
    **Watchlist commands**
    
    ```
    // Add a new watchlist 
    POST kbn:/api/entity_analytics/watchlists
    {
        "name": "test-name",
        "description": "A",
        "riskModifier": 1
    }
    // List all watchlists 
    GET kbn:/api/entity_analytics/watchlists/list
    
    ```
    #### Unhappy Path - Error and Prvileges States 
    1. **Privileges** - Create a user with a role which has access to
    security but no access to any of these indicies:
    ```
    Elasticsearch index privileges (read):
    * .entity_analytics.privileged_monitoring.users-<spaceId>
    * risk-score.risk-score-*
    * .alerts-security.alerts-<spaceId>
    * .ml-anomalies-*
    ```
    4. **Error state** - easiest way to do this is to first add the
    watchlists as seen above, then open dev tools and block the list request
    for watchlists, reload the page and ensure the error message shows after
    the loading state.
    
    ### Visibility constraints
    - It should be hidden when `entityAnalyticsWatchlistEnabled` is not
    enabled
    - It should be visible when the user has access to the entity analytics
    feature
    - It should be visible for platinum users
    
    ### NB:
    - Privileges route will be updated to use watchlists, using same as
    privmon until then.
    https://github.com/elastic/security-team/issues/16102

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/hooks/use_watchlists_privileges.ts
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/hooks/use_watchlists_privileges.ts
@@ -4,17 +4,18 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { useQuery } from '@kbn/react-query';
+
 import type { SecurityAppError } from '@kbn/securitysolution-t-grid';
+import { useQuery } from '@kbn/react-query';
 import type { PrivMonPrivilegesResponse } from '../../../../common/api/entity_analytics';
 import { useEntityAnalyticsRoutes } from '../api';
 
-export const usePrivilegedMonitoringPrivileges = () => {
-  const { fetchPrivilegeMonitoringPrivileges } = useEntityAnalyticsRoutes();
-  // TODO: remove this hook when privmon UI is retired. https://github.com/elastic/security-team/issues/16105
+// TODO: update to WATCHLISTS privileges route when backend is implemented; https://github.com/elastic/security-team/issues/16102
+export const useWatchlistsPrivileges = () => {
+  const { fetchWatchlistPrivileges } = useEntityAnalyticsRoutes();
   return useQuery<PrivMonPrivilegesResponse, SecurityAppError>({
-    queryKey: ['GET', 'FETCH_PRIVILEGED_MONITORING_PRIVILEGES'],
-    queryFn: fetchPrivilegeMonitoringPrivileges,
+    queryKey: ['GET', 'FETCH_WATCHLIST_PRIVILEGES'],
+    queryFn: fetchWatchlistPrivileges,
     retry: 0,
   });
 };
